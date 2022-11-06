e3d29add3e85ae444ffe97df2d4e7593ebaaf78d
thanhlong0949 <longnguyenthanh0949@gmail.com>
2022-11-07 05:12:52 +0700

    update client

--- lib/client.dart
+++ lib/client.dart
@@ -1,29 +1,13 @@
 import 'package:gql_http_link/gql_http_link.dart';
 import 'package:ferry/ferry.dart';
-import 'package:ferry_hive_store/ferry_hive_store.dart';
-// import 'package:hive/hive.dart';
-// *** If using flutter ***
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:capstone_laundry_client/src/__generated__/schema.schema.gql.dart'
-    show possibleTypesMap;
 
-Future<Client> initClient() async {
-  //Hive.init('hive_data');
-  // OR, if using flutter
-  await Hive.initFlutter();
+export 'src/graphql/__generated__/equipment.data.gql.dart';
+export 'src/graphql/__generated__/equipment.req.gql.dart';
+export 'src/graphql/__generated__/equipment.var.gql.dart';
+export 'src/__generated__/schema.schema.gql.dart';
 
-  final box = await Hive.openBox("graphql");
-
-  final store = HiveStore(box);
-
-  final cache = Cache(store: store, possibleTypes: possibleTypesMap);
-
-  final link = HttpLink('http://128.199.110.106:8080/v1/graphql');
-
-  final client = Client(
-    link: link,
-    cache: cache,
-  );
-
+Client initClient(String uri) {
+  final link = HttpLink(uri);
+  final client = Client(link: link);
   return client;
 }
