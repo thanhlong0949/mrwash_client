42a44dcb1ab67576b24f0033b80e9dc8b845e923
thanhlong0949 <longnguyenthanh0949@gmail.com>
2022-11-06 11:56:29 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/capstone_laundry_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.capstone_laundry_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/__generated__/serializers.gql.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/serializer.dart';
+import 'package:built_value/standard_json_plugin.dart' show StandardJsonPlugin;
+import 'package:ferry_exec/ferry_exec.dart';
+import 'package:gql_code_builder/src/serializers/operation_serializer.dart'
+    show OperationSerializer;
+import 'package:path/to/date_serializer.dart' show DateSerializer;
+
+part 'serializers.gql.g.dart';
+
+final SerializersBuilder _serializersBuilder = _$serializers.toBuilder()
+  ..add(OperationSerializer())
+  ..add(DateSerializer())
+  ..addPlugin(StandardJsonPlugin());
+@SerializersFor([])
+final Serializers serializers = _serializersBuilder.build();
